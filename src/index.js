81f98cdd4244454fdb5b796e4f3086ba11084886
Akshay <98652196+akshayg-08@users.noreply.github.com>
2025-03-22 12:58:50 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import './style.css'
+import CounterApp from './CounterApp'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <CounterApp/>
+)
